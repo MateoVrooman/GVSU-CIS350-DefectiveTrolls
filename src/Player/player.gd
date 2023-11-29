478cf78949d3f41229701f4901de68705eaca2d3
h1nes4l <hinesal@mail.gvsu.edu>
2023-11-29 15:33:44 -0500

    Enemy AI working, combat working excluding death animations and enemy map collision

--- src/Player/player.gd
+++ src/Player/player.gd
@@ -9,15 +9,18 @@ extends CharacterBody2D
 @onready var idleSprite = $Idle
 @onready var attackSprite = $Attack
 
-
+var enemyInAttackRange = false
+var enemyAttackCooldown = true 
+var health = 100
+var playerAlive = true
 var direction = "Down"
 var attacking: bool = false
+var attackIP = false			 #Attack in progress
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed	
 	if Input.is_physical_key_pressed(KEY_SPACE):
-		print("Attacking")
 		attack()
 
 func hideSprites():
@@ -32,6 +35,8 @@ func attack():
 	attacking = true
 	if animations.get_current_animation() != ("attack" + direction):
 		hideSprites()
+		global.playerCurrentlyAttacking = true
+		attackIP = true
 		print("attack initiated")
 		attackSprite.show()
 		animations.play("attack" + direction)   
@@ -62,12 +67,61 @@ func updateAnimation(direction):
 	return direction
 
 func _physics_process(delta):
+	enemyAttack()
 	handleInput()
+	updateHealth()
+	
 	if !attacking:
 		move_and_slide()
 	direction = updateAnimation(direction)
+	
+	if health <= 0:
+		playerAlive = false
+		health = 0
+		print("player has been killed")
+		self.queue_free()
 
 
 func _on_animation_player_animation_finished(animation):
 	if animation == ("attack" + direction):
 		attacking = false
+
+func player():
+	pass
+
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("enemy"):
+		enemyInAttackRange = true
+
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("enemy"):
+		enemyInAttackRange = false
+
+func enemyAttack():
+	if enemyInAttackRange and enemyAttackCooldown == true:
+		health = health - 20
+		enemyAttackCooldown = false
+		$AttackCooldown.start()
+		print(health)
+
+
+func _on_attack_cooldown_timeout():
+	enemyAttackCooldown = true
+
+func updateHealth():
+	var healthBar = $HealthBar
+	healthBar.value = health
+	
+	if health >= 100:
+		healthBar.visible = false
+	else:
+		healthBar.visible = true
+
+func _on_regen_timer_timeout():
+	if health < 100 and !attacking:
+		health = health + 20
+		if health > 100:
+			health = 100
+	if health <= 0:
+		health = 0
+	
--- src/enemy1/enemy.gd
+++ src/enemy1/enemy.gd
@@ -4,17 +4,90 @@ var speed: int = 150
 var player_chase = false
 var player = null
 
+var health = 100
+var playerInAttackZone = false
+var canTakeDamage = false
+
+func _ready():
+	$AnimatedSprite2D.play("idle")
+
+
 func _physics_process(delta):
+	enemyTakeDamage()
+	updateHealth()
+	
 	if player_chase:
 		position += (player.position - position)/speed
-
+		$AnimatedSprite2D.play("walk")
+		
+		if(player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else :
+			$AnimatedSprite2D.flip_h = false
+			
 
 func _on_detection_area_body_entered(body):
 	player = body
+	$AnimatedSprite2D.play("walk")
 	player_chase = true 
-	
 
 
 func _on_detection_area_body_exited(body):
 	player = null
+	$AnimatedSprite2D.play("idle")
 	player_chase = false
+
+func enemy():
+	pass
+
+
+func _on_enemy_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_chase = false
+		playerInAttackZone = true
+		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play("attack")
+		canTakeDamage = true
+
+func _on_enemy_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_chase = true
+		playerInAttackZone = false
+		$AnimatedSprite2D.play("walk")
+		canTakeDamage = false
+		
+func enemyTakeDamage():
+	if playerInAttackZone and global.playerCurrentlyAttacking == true:
+		if canTakeDamage == true:
+			health = health - 20
+			$TakeDamageCooldown.start()
+			canTakeDamage = false
+			print ("enemy health = ", health)
+			enemyDeath()
+			
+
+func _on_take_damage_cooldown_timeout():
+	canTakeDamage = true
+	
+func updateHealth():
+	var enemyHealthBar = $EnemyHealthBar
+	
+	enemyHealthBar.value = health
+	
+	if health >= 100:
+		enemyHealthBar.visible = false
+	else:
+		enemyHealthBar.visible = true
+
+func enemyDeath():
+	if health <= 0:
+		health = 0
+		playerInAttackZone = false
+		player_chase = false
+		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play("death")
+		$DeathAnimTimer.start()
+		if $DeathAnimTimer.is_stopped():
+			self.queue_free()
+
+
